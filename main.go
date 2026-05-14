d62fb8439423d8984704c5633cc9680dc4318dfc
zolet-hash <emmanuelsulwe@gmail.com>
2026-05-14 12:48:13 +0300

    Add basic argument validation and usage instructions for portscanner
    - Checks for correct number of command-line arguments.
    - Prints usage instructions and available options (-p, -u, -t, --help) if input is invalid.
    - Provides a foundation for future TCP/UDP port scanning implementation.

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	// tinyscanner -u 20.78.06.364 -p 80
+	if len(os.Args) != 5 {
+		fmt.Fprintln(os.Stderr, "Usage: portscanner [OPTIONS] [HOST] [PORT]")
+		fmt.Fprintln(os.Stderr, "Options:")
+		fmt.Fprintln(os.Stderr, "-p               Range of ports to scan")
+		fmt.Fprintln(os.Stderr, "-u               UDP scan")
+		fmt.Fprintln(os.Stderr, "-t               TCP scan")
+		fmt.Fprint(os.Stderr, "--help           Show this message and exit.")
+		return
+	}
+}
