39327219184d30f1bb5e75b6387d65fb288d9c39
zolet-hash <emmanuelsulwe@gmail.com>
2026-05-16 14:40:28 +0300

    Add ScanHost function which utilizes go's concurrency to scan host IPs - Acquires a semaphore slot before launching a goroutine - waits for all goroutines to finish then closes all channels - appends the list of all open ports in openPorts slice - sorts all the ports array inside the openPorts slice in ascending order - returns the sorted array of open ports

--- tcp/tcp.go
+++ tcp/tcp.go
@@ -3,6 +3,7 @@ package tcp
 import (
 	"fmt"
 	"net"
+	"sort"
 	"sync"
 	"time"
 )
@@ -15,7 +16,7 @@ func ScanPortConcurrently(host string, port int, timeout time.Duration,
 
 	defer func() { <-sem }()
 
-	address := fmt.Sprintf("%s %d", host, port)
+	address := fmt.Sprintf("%s:%d", host, port)
 	conn, err := net.DialTimeout("tcp4", address, timeout)
 	if err != nil {
 		return
@@ -26,5 +27,27 @@ func ScanPortConcurrently(host string, port int, timeout time.Duration,
 
 //ScanHost
 func ScanHost(host string, startPort,endPort,maxConcurrent int, timeout time.Duration) []int {
+	results := make(chan int, endPort-startPort+1)
+	sem := make(chan struct{}, maxConcurrent) //Limit concurrent connection attempts
+
+	var wg sync.WaitGroup
+
+	for port := startPort; port <= endPort; port++ {
+		sem <-struct{}{} //Acquire a semaphore slot before launching the groutine
+		wg.Add(1)
+		go ScanPortConcurrently(host, port, timeout, results, &wg, sem,)
+	}
+
+	// wait for all goroutines to finish then close all channels
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
 	
+	var openPorts []int
+	for port := range results{
+		openPorts = append(openPorts, port)
+	}
+	sort.Ints(openPorts)
+	return openPorts
 }
