72008b19fde3f4d1dcdc858df8b71f1783ea719a
zolet-hash <emmanuelsulwe@gmail.com>
2026-05-19 13:18:28 +0300

    Implement a ScanUDPPort function which scans open ports using UDP

--- tcp/udp.go
+++ tcp/udp.go
@@ -3,11 +3,16 @@ package tcp
 import (
 	"fmt"
 	"net"
-	"time"
 )
 
-func ScanUDPPort(host string, port int, timeout time.Duration) {
-	address := fmt.Sprintf("%s:%d", host, port)
-	conn, err := net.DialUDP("udp", nil, addre)
+func ScanUDPPort(host string, port int) bool {
+	address := fmt.Sprintf("%s%d", host, port)
+	conn, err := net.Dial("udp4", address)
+
+	if err != nil {
+		return false
+	}
+	conn.Close()
+	return true
 
 }
