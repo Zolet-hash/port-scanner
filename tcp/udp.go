14e21dcdf1c7b624760bba4a8f18bfd36c4131bd
zolet-hash <emmanuelsulwe@gmail.com>
2026-05-19 13:51:40 +0300

    add Documentation of the function

--- tcp/udp.go
+++ tcp/udp.go
@@ -5,11 +5,13 @@ import (
 	"net"
 )
 
+// ScanUDPPort scans open ports using UDP port scanning technique
 func ScanUDPPort(host string, port int) bool {
-	address := fmt.Sprintf("%s%d", host, port)
-	conn, err := net.Dial("udp4", address)
+	address := fmt.Sprintf("%s:%d", host, port)
+	conn, err := net.Dial("udp", address)
 
 	if err != nil {
+		fmt.Println("Error:", err)
 		return false
 	}
 	conn.Close()
